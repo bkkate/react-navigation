6202ea4ee071bcb0622cd0789e9f535214cbac4f
Kate Kim <bkkatekim1@gmail.com>
2023-07-05 16:05:34 -0400

    modal popup

---
+++ src/pages/ModalPage.js
@@ -0,0 +1,38 @@
+import Modal from "../components/Modal";
+import Button from "../components/Button";
+import { useState } from "react";
+
+function ModalPage() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClick = () => {
+    setShowModal(true);
+  };
+
+  const handleClose = () => {
+    setShowModal(false);
+  }
+
+  const actionBar=(
+    <div>
+        <Button onClick={handleClose} primary>I Accept!</Button>
+    </div>
+  )
+
+  const modal = <Modal onClose={handleClose} actionBar={actionBar}>
+
+    </Modal>  
+    ;
+
+  return (
+    <div>
+      <Button onClick={handleClick} primary>
+        Open Modal
+      </Button>
+      {showModal && modal}
+      
+    </div>
+  );
+}
+
+export default ModalPage;
